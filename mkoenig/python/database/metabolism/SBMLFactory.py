2ec14399d8517835855e1645a27f160ad0397325
Matthias Koenig <konigmatt@googlemail.com>
2015-03-09 19:15:06 +0100

    Starting SBML from Database

---
+++ mkoenig/python/database/metabolism/SBMLFactory.py
@@ -0,0 +1,15 @@
+'''
+Write SBML for metabolism process directly from database information.
+
+@author: Matthias Koeng
+@date: 2015-03-09
+'''
+from libsbml import *
+from public.models import Metabolite, Reaction, Protein, ProteinComplex
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
