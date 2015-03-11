7280a4950f79a922b8447a05f8e65067914763d9
Matthias Koenig <konigmatt@googlemail.com>
2015-03-11 10:56:13 +0100

    Updated requirement files

---
+++ mkoenig/python/metabolism/fba/FluxBoundsInformation.py
@@ -0,0 +1,40 @@
+'''
+Collect all the required information to calculate the flux bounds.
+
+
+Created on Mar 11, 2015
+@author: mkoenig
+
+Information required
+- thermodynamically irreversible
+- chemically catalyzed reaction 
+- all enzymes which catalyze reaction (ne, kcat_f, kcat_b)
+
+-  chemical reactions (protein substrates, np)
+
+internal exchange reactions (nm)
+
+external exchange reactions 
+- m*k-ex, m*k+ex (exchange rates)
+
+- cell dry mass m (mass)
+5397 State State_Mass Cell mass
+
+
+- GAM
+- used energy
+
+What is the difference between enzymes and proteins?
+
+
+
+'''
+
+def calc_requirements():
+    pass
+
+
+if __name__ == '__main__':
+    calc_requirements()
+
+
--- mkoenig/python/metabolism/sbml/InputOutputRequirements.py
+++ mkoenig/python/metabolism/sbml/InputOutputRequirements.py
@@ -11,9 +11,10 @@ from django.core.exceptions import ObjectDoesNotExist
 from public.models import Protein, EnzymeParticipant, Entry
 from public.models import Reaction as DBReaction 
 
+from pandas import DataFrame
+
 # Create list of input-output requirements
 def calculate_requirements(m):
-    from pandas import DataFrame
     print '*** Requirements ***'
     # list
     cols = ['id', 'model_type', 'name', 'readonly', 'readwrite', 'requirement']
@@ -30,7 +31,11 @@ def calculate_requirements(m):
         try:
             # Check if entry
             e = Entry.objects.get(wid=wid)
-            info.append(['__'.join([wid, cid]), e.model_type, e.name, '', 'x', 'x'])
+            if (cid != 'c'):
+                info.append(['__'.join([wid, cid]), e.model_type, e.name, '', 'x', 'x'])
+            else: 
+                info.append([wid, e.model_type, e.name, '', 'x', 'x'])
+            
             
         except ObjectDoesNotExist:
             print 'No wid', sid, wid
@@ -74,6 +79,7 @@ if __name__ == '__main__':
     print info_df.head(10)
     info_df.to_csv(out_csv, sep='\t',encoding='UTF8')
     
+    # write csvs
     for name in ['Metabolite', 'ProteinMonomer', 'ProteinComplex']:
         f_csv = os.path.join(RESULTS_DIR, 'Molecules_names_{}_Metabolism.csv'.format(name))
         # write the subsets
@@ -81,11 +87,11 @@ if __name__ == '__main__':
         print df.head()
         df = df[['id', 'name', 'readonly', 'readwrite', 'requirement']]
         df = df.drop_duplicates()
+        import csv
+        df.to_csv(f_csv, sep=',',encoding='UTF8', index=False, quoting=csv.QUOTE_ALL)
+    
         
-        df.to_csv(f_csv, sep='\t',encoding='UTF8', index=False)
-        
-        
-        
-        
-
-     
+    # parameters changed  
+    f_csv = os.path.join(RESULTS_DIR, 'Parameters_Metabolism.csv'.format(name))
+    df = DataFrame()
+    df.to_csv(f_csv, sep=',',encoding='UTF8', index=False, quoting=csv.QUOTE_ALL)
