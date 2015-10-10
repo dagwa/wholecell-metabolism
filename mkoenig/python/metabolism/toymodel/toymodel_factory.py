e32461f32e1e813d560af24269d022288783cd26
Matthias Koenig <konigmatt@googlemail.com>
2015-10-10 22:56:12 +0200

    First toy model

--- mkoenig/python/metabolism/toymodel/toymodel_factory.py
+++ mkoenig/python/metabolism/toymodel/toymodel_factory.py
@@ -55,6 +55,88 @@ s_C = createSpecies(id="C", name="C", initialAmount=0, constant=False,
 # Create paramters
 
 # create reactions
+def createReaction(id, name, fast=False, reversible=True,
+                     reactants={}, products={}):
+    r = model.createReaction()
+    r.setId(id)
+    r.setName(name)
+    r.setFast(fast)
+    r.setReversible(reversible)
+
+    for sid, stoichiometry in reactants.iteritems():
+        rt = r.createReactant()
+        rt.setSpecies(sid)
+        rt.setStoichiometry(abs(stoichiometry))
+        rt.setConstant(True)
+
+    for sid, stoichiometry in products.iteritems():
+        rt = r.createProduct()
+        rt.setSpecies(sid)
+        rt.setStoichiometry(abs(stoichiometry))
+        rt.setConstant(True)
+
+createReaction(id="R1", name="R1", fast=False, reversible=True,
+               reactants={"A": 1}, products={"B1": 1})
+createReaction(id="R2", name="R2", fast=False, reversible=True,
+               reactants={"B1": 1}, products={"B2": 1})
+createReaction(id="R3", name="R3", fast=False, reversible=True,
+               reactants={"B2": 1}, products={"C": 1})
 
 # create objective function
+objective = mplugin.createObjective()
+objective.setId("R3_maximize")
+objective.setType("maximize");
+mplugin.setActiveObjectiveId("maximize");
+fluxObjective = objective.createFluxObjective()
+fluxObjective.setReaction("R3")
+fluxObjective.setCoefficient(1.0)
 
+"""
+        # <fluxbounds>
+        # parameters for dynamical calculation of flux bounds
+        for p_name in ('lb_fbaReactionBounds', 'ub_fbaReactionBounds', 'lb_fbaEnzymeBounds', 'ub_fbaEnzymeBounds'):
+            par = model.createParameter()
+            par.setId('{}__{}'.format(index, p_name))
+            par.setValue(r_fba_df[p_name][index])
+            par.setConstant(True)
+        # The reaction flux bounds are set as hard upper and lower flux bounds
+        # These are NOT the dynamical flux bounds.
+
+        for p_name in ('lb_fbaReactionBounds', 'ub_fbaReactionBounds'):
+            # "lessEqual", "greaterEqual", "equal"
+            bound = mplugin.createFluxBound();
+            bound.setReaction(index);
+            bound.setValue(r_fba_df[p_name][index])
+            if p_name.startswith('lb'):
+                bound.setId('lb__{}'.format(index))
+                bound.setOperation("greaterEqual")
+            if p_name.startswith('ub'):
+                bound.setId('ub__{}'.format(index))
+                bound.setOperation("lessEqual")
+
+    # <objective function>
+
+"""
+# write sbml
+fba_out = "fba_toy.xml"
+writer = SBMLWriter()
+writer.writeSBML(doc_fba, fba_out)
+
+from sbml_tools.checks import check_sbml
+check_sbml(fba_out)
+
+#########################################################################################3
+# simulate the FBA model
+
+import cobra
+cobra_model = cobra.io.read_sbml_model(fba_out)
+
+
+### Running FBA
+cobra_model.optimize()
+print cobra_model.solution.status
+# Output:
+# 'optimal'
+print cobra_model.solution.f
+{reaction: reaction.objective_coefficient for reaction in cobra_model.reactions
+ if reaction.objective_coefficient > 0}
