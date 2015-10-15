8719e745515b110692e3aefafaa2b69425e2e0d6
Matthias Koenig <konigmatt@googlemail.com>
2015-10-15 05:24:01 +0200

    external model definitions and toy models

--- mkoenig/python/metabolism/toymodel/toymodel_comp.py
+++ mkoenig/python/metabolism/toymodel/toymodel_comp.py
@@ -9,33 +9,48 @@ from libsbml import *
 from toymodel_settings import toy_comp_file
 from toymodel_factory import *
 
+
+def create_ExternalModelDefinition(mdoc, cid, sbml_file):
+    extdef = mdoc.createExternalModelDefinition()
+
+    extdef.setId(cid)
+    extdef.setName(cid)
+    extdef.setModelRef(cid)
+    extdef.setSource(sbml_file)
+    return extdef
+
+
 def create_comp_model(sbml_file):
 
     sbmlns = SBMLNamespaces(3, 1, "comp", 1)
     doc = SBMLDocument(sbmlns)
-    doc.setPackageRequired("comp", False)
+    doc.setPackageRequired("comp", True)
     mdoc = doc.getPlugin("comp")
 
-    model = doc.createModel()
-    mplugin = model.getPlugin("comp")
-
     # create listOfExternalModelDefinitions
-    extdef = CompSBMLDocumentPlugin.createExternalModelDefinition(mdoc)
-    extdef.setId("toy_ode_bounds")
-    extdef.setName("toy_ode_bounds")
-    extdef.setModelRef("toy_ode_bounds")
-    extdef.setSource("toy_ode_bounds.xml")
+    emd_bounds = create_ExternalModelDefinition(mdoc, "toy_ode_bounds", sbml_file="toy_ode_bounds.xml")
+    emd_update = create_ExternalModelDefinition(mdoc, "toy_ode_update", sbml_file="toy_ode_update.xml")
+    emd_model = create_ExternalModelDefinition(mdoc, "toy_ode_model", sbml_file="toy_ode_model.xml")
 
-
-    # add listOfSubmodels which reference the External models
-
-    # add list of Ports
-
-
-    # model
+    # create models and submodels
     model = doc.createModel()
     model.setId("toy_ode_comp")
     model.setName("Combined ODE/SSA model")
+    mplugin = model.getPlugin("comp")
+
+    # add listOfSubmodels which reference the External models
+    submodel_bounds = mplugin.createSubmodel()
+    submodel_bounds.setId("submodel_bounds")
+    submodel_bounds.setModelRef(emd_bounds.getModelRef())
+
+    submodel_update = mplugin.createSubmodel()
+    submodel_update.setId("submodel_update")
+    submodel_update.setModelRef(emd_update.getModelRef())
+
+    submodel_model = mplugin.createSubmodel()
+    submodel_model.setId("submodel_model")
+    submodel_model.setModelRef(emd_model.getModelRef())
+
 
     # write SBML file
     write_and_check(doc, sbml_file)
